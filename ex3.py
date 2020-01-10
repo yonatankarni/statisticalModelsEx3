1f57a654eeca0afb4905699df1f584c75368b899
Yonatan Karni <ykarni@outbrain.com>
2020-01-10 21:41:40 +0200

    ex3 4x faster now by calculating the Pik denominator just once for each cluster

--- ex3.py
+++ ex3.py
@@ -3,6 +3,11 @@ from time import time
 import numpy as np
 
 
+underflow_k = 10
+epsilon = 1e-10
+pik_lambda = 1e-10
+
+
 # calc_wti: calculate the probability for cluster i given doc t
 # i: cluster index (0..8)
 # clusters_probabilities - array, i-th place has the probability of a document belonging to cluster i (before we look
@@ -11,23 +16,29 @@ import numpy as np
 # yt - the th document (a counter over the document words)
 def calc_wti(i, yt, clusters_probabilities, clusters_word_probabilities):
     n = len(clusters_probabilities)
-    alpha_i = clusters_probabilities[i]
-    document_piks = [pow(clusters_word_probabilities[word][i], cnt) for word, cnt in yt.most_common()]
-    numerator = alpha_i * np.prod(document_piks)
-    denom_list = np.zeros(n)
+
+    z_values = np.zeros(n)
     for j in range(n):
         alpha_j = clusters_probabilities[j]
-        cluster_document_piks = [pow(clusters_word_probabilities[word][j], cnt) for word, cnt in yt.most_common()]
-        denom_list[j] = alpha_j * np.prod(cluster_document_piks)
-    denominator = np.sum(denom_list)
+        zj = np.log(alpha_j) + np.sum([nt_k * np.log(clusters_word_probabilities[word][j]) for word, nt_k in yt.most_common()])
+        z_values[j] = zj
 
-    return numerator / denominator
+    m = np.amax(z_values)
+
+    if z_values[i] - m < -underflow_k:
+        return 0
+    else:
+        denom_valid_zjs = filter(lambda zj: zj - m >= -underflow_k, z_values)
+        denom_list = list(map(lambda zj: np.power(np.e, zj - m), denom_valid_zjs))
+        denominator = np.sum(denom_list)
+        numerator = np.power(np.e, z_values[i] - m)
+        return numerator / denominator
 
 
 # returns a list of size N (the number of documents), each list element represents the probability distribution over
 # the clusters, so list of lists
 def e_step(documents, clusters_probabilities, clusters_word_probabilities):
-    return [[calc_wti(i, doc, clusters_probabilities, clusters_word_probabilities) for i in range(len(clusters_probabilities))]
+    return [np.array([calc_wti(i, doc, clusters_probabilities, clusters_word_probabilities) for i in range(len(clusters_probabilities))])
             for doc in documents]
 
 
@@ -39,45 +50,55 @@ def calc_ai(cluster_idx, wts):
 # word_k - a word
 # cluster_idx - the cluster # (0-8)
 # documents - list of counters (with word frequencies for each document)
-def calc_pik(word_k, cluster_idx, wts, documents):
+def calc_pik(word_k, cluster_idx, wts, pik_cluster_denominators, documents, vocab_size):
     numerator = np.sum([wts[t][cluster_idx]*document[word_k] for t, document in enumerate(documents)])
-    denominator = np.sum([wts[t][cluster_idx]*sum(document.values()) for t, document in enumerate(documents)])
-    return numerator / denominator
+    denominator = pik_cluster_denominators[cluster_idx]
+    return (numerator + pik_lambda) / (denominator + vocab_size*pik_lambda)
 
 
-def m_step(wts, documents, vocab):
+def m_step(wts, documents, vocab, vocab_size):
     cluster_count = len(wts[0])
-    clusters_probabilities = [calc_ai(cluster_idx, wts) for cluster_idx in range(cluster_count)]
+    clusters_probabilities = np.array([calc_ai(cluster_idx, wts) for cluster_idx in range(cluster_count)])
+    clusters_probabilities_with_epsilon_threshold = np.array([c if c > epsilon else epsilon for c in clusters_probabilities])
+    clusters_probabilities_sum = np.sum(clusters_probabilities_with_epsilon_threshold)
+    smoothed_clusters_probabilities = clusters_probabilities_with_epsilon_threshold / clusters_probabilities_sum
 
-    clusters_word_probabilities = {word_k: [calc_pik(word_k, cluster_idx, wts, documents) for cluster_idx in range(cluster_count)] for word_k in vocab}
-    return clusters_probabilities, clusters_word_probabilities
+    pik_cluster_denominators = np.array([np.sum([wts[t][cluster_idx] * sum(document.values()) for t, document in enumerate(documents)]) for cluster_idx in range(cluster_count)])
+    # clusters_word_probabilities = [(word_k, np.array(
+    #     [calc_pik(word_k, cluster_idx, wts, pik_cluster_denominators, documents, vocab_size) for cluster_idx in
+    #      range(cluster_count)])) for word_k in vocab]
+    clusters_word_probabilities = {word_k: np.array([calc_pik(word_k, cluster_idx, wts, pik_cluster_denominators, documents, vocab_size) for cluster_idx in range(cluster_count)]) for word_k in vocab}
+    return smoothed_clusters_probabilities, clusters_word_probabilities
 
 
 def load_input(input_filename):
     with open(input_filename, 'r') as development_set_file:
         lines = development_set_file.readlines()
 
-    vocab = Counter([word for line in lines[1::2] for word in line.strip().split(" ")]).keys()
+    vocab_counter = Counter([word for line in lines[1::2] for word in line.strip().split(" ")])
+    vocab = set(map(lambda p: p[0], filter(lambda x: x[1] > 3, vocab_counter.most_common())))
 
-    zipped = list(zip(lines[0::2], lines[1::2]))
+    documents_after_filtering = [Counter(list(filter(lambda w: w in vocab, line.strip().split(" ")))) for line in lines[1::2]]
 
-    documents_info = list(map(lambda x: (x[0], x[1].strip().split(" "), Counter(x[1].strip().split(" "))), zipped))
+    document_info = lines[0::2]
+    documents_info = list(zip(document_info, documents_after_filtering))
+
     return documents_info, vocab
 
 
 if __name__ == "__main__":
     documents_info, vocab = load_input("dataset/develop.txt")
-
-    documents = list(map(lambda x: x[2], documents_info))
+    documents = list(map(lambda x: x[1], documents_info))
 
     # initialize - fake e step where each document gets a cluster by calculating the document index modulo 9
     EYE = np.eye(9)
     wts = [EYE[t % 9] for t in range(len(documents))]
 
+    vocab_size = len(vocab)
     # for i in range(1):
     start_time = time()
 
-    clusters_probabilities, clusters_word_probabilities = m_step(wts, documents, vocab)
+    clusters_probabilities, clusters_word_probabilities = m_step(wts, documents, vocab, vocab_size)
     wts = e_step(documents, clusters_probabilities, clusters_word_probabilities)
 
     print("run took " + str((time() - start_time)) + " seconds")
--- ex3_test.py
+++ ex3_test.py
@@ -4,16 +4,20 @@ from ex3 import calc_wti, calc_ai, e_step, m_step
 
 # initial values - not necessarily completely coherent
 clusters_probabilities = [0.4, 0.6]
-clusters_word_probabilities = [
-    {"aaa": 0.2, "bbb": 0.7, "ccc": 1},
-    {"aaa": 0.5, "bbb": 0.1, "ccc": 0.4}
-]
+clusters_word_probabilities = {
+    "aaa": [0.2, 0.5],
+    "bbb": [0.7, 0.1],
+    "ccc": [1, 0.4]
+}
+
 documents = [Counter(["aaa", "bbb", "bbb", "ccc", "ccc", "ccc"]), Counter(["aaa", "aaa", "bbb", "ccc"])]
 
 # test calc_wti
 y0 = Counter(["aaa", "aaa", "bbb", "bbb"])
 wt0_0 = calc_wti(0, y0, clusters_probabilities, clusters_word_probabilities)
 wt0_1 = calc_wti(1, y0, clusters_probabilities, clusters_word_probabilities)
+print(wt0_0)
+print(wt0_1)
 assert 0.99999 < wt0_0 + wt0_1 < 1.00001
 
 y1 = Counter(["aaa", "ccc"])
@@ -33,4 +37,4 @@ clusters_probabilities = [1/9. for x in range(2)]
 wts = e_step(documents, clusters_probabilities, clusters_word_probabilities)
 
 vocab = ["aaa", "bbb", "ccc"]
-clusters_probabilities, clusters_word_probabilities = m_step(wts, documents, vocab)
+clusters_probabilities, clusters_word_probabilities = m_step(wts, documents, vocab, len(vocab))
